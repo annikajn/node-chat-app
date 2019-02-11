b0ce2ca0bdffae9a0e9cccc46cfa8d35c7355340
annikanic <14ajn@queensu.ca>
2019-02-11 16:29:14 -0500

    Setting up socket ios

--- server/server.js
+++ server/server.js
@@ -1,14 +1,26 @@
 require('./config/config');
-
+const socketIO = require('socket.io');
+const http = require('http');
 const path = require('path');
 const publicpath = path.join(__dirname, '../public');
 const express = require('express');
 const port = process.env.PORT;
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
+// var client  
 
 app.use(express.static(publicpath));
 
-app.listen(port, () => {
+io.on('connection', (socket) =>{
+    console.log('New user connected');
+
+    socket.on('disconnect', ()=>{
+        console.log('User disconnected');
+    });
+});
+
+server.listen(port, () => {
     console.log(`Started up at port ${port}`);
   });
   
